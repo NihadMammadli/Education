5bc94149d4b9a201d19dd03a7a58b511708399c0
NihadMammadli <nmammadli14004@ada.edu.az>
2024-07-08 20:35:42 +0400

    High schools changed too

--- src/Pages/HighSchools/index.jsx
+++ src/Pages/HighSchools/index.jsx
@@ -17,34 +17,26 @@ function App() {
             key: 'name',
         },
         {
-            title: <ColumnHeader header='Creation Date' onFilter={handleFilter} />,
-            dataIndex: 'creation_date',
-            key: 'creation_date',
+            title: <ColumnHeader header='Location' onFilter={handleFilter} />,
+            dataIndex: 'location',
+            key: 'location',
         },
         {
-            title: <ColumnHeader header='Number of Faculties' onFilter={handleFilter} />,
-            dataIndex: 'faculty_number',
-            key: 'faculty_number',
-        },
-        {
-            title: <ColumnHeader header='Global Ranking' onFilter={handleFilter} />,
-            dataIndex: 'global_ranking',
-            key: 'global_ranking',
+            title: <ColumnHeader header='Students Enrolled' onFilter={handleFilter} />,
+            dataIndex: 'students_enrolled',
+            key: 'students_enrolled',
         },
         {
             title: <ColumnHeader header='Programs Offered' onFilter={handleFilter} />,
-            dataIndex: 'offered_programs',
-            key: 'offered_programs',
-            render: (_, { offered_programs }) => (
+            dataIndex: 'type',
+            key: 'type',
+            render: (_, { type }) => (
                 <>
-                    {offered_programs?.map((program) => {
-                        let color = program.length > 5 ? 'geekblue' : 'green';
-                        if (program === 'loser') {
-                            color = 'volcano';
-                        }
+                    {type?.map((tip) => {
+                        let color = tip.length > 6 ? 'red' : 'blue';
                         return (
-                            <Tag color={color} key={program}>
-                                {program}
+                            <Tag color={color} key={tip}>
+                                {tip}
                             </Tag>
                         );
                     })}
@@ -54,7 +46,7 @@ function App() {
     ];
 
     useEffect(() => {
-        axios.get("https://668be99a0b61b8d23b0baf7e.mockapi.io/api/education/universities").then((res) => {
+        axios.get("https://668be99a0b61b8d23b0baf7e.mockapi.io/api/education/high_schools").then((res) => {
             setUniversities(res?.data)
         }).catch((error) => {
             console.error(error)
