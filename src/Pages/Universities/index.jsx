d4cbd25969e8193818350962f3aa1137b257534c
NihadMammadli <nmammadli14004@ada.edu.az>
2024-07-08 20:05:41 +0400

    Universities done for now

--- src/Pages/Universities/index.jsx
+++ src/Pages/Universities/index.jsx
@@ -1,10 +1,69 @@
+import { Table, Tag } from "antd";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react"
+import axios from "axios";
+
 
 function App() {
+
+    const [universities, setUniversities] = useState([])
+
+    const columns = [
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: 'Creation Date',
+            dataIndex: 'creation_date',
+            key: 'creation_date',
+        },
+        {
+            title: 'Number of Faculties',
+            dataIndex: 'faculty_number',
+            key: 'faculty_number',
+        },
+        {
+            title: 'Global Ranking',
+            dataIndex: 'global_ranking',
+            key: 'global_ranking',
+        },
+        {
+            title: 'Programs Offered',
+            dataIndex: 'offered_programs',
+            key: 'offered_programs',
+            render: (_, { offered_programs }) => (
+                <>
+                    {offered_programs.map((program) => {
+                        let color = program.length > 5 ? 'geekblue' : 'green';
+                        if (program === 'loser') {
+                            color = 'volcano';
+                        }
+                        return (
+                            <Tag color={color} key={program}>
+                                {program} 
+                            </Tag>
+                        );
+                    })}
+                </>
+            ),
+        },
+    ];
+
+    useEffect(() => {
+        axios.get("https://668be99a0b61b8d23b0baf7e.mockapi.io/api/education/universities").then((res) => {
+            setUniversities(res?.data)
+        }).catch((error) => {
+            console.error(error)
+        })
+    }, [])
+
     return (
         <>
-            Universities
+            <Table dataSource={universities} columns={columns} />
         </>
-    )
+    );
 }
 
-export default App
+export default App;
